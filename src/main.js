f90e6841bda5809edcad252825705af78672ef54
Leandro Da Souza <dasouza.leandro@gmail.com>
2020-03-18 14:15:14 +0100

    init project with vue router and vuex

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import VueRouter from 'vue-router';
+import store from './store/store';
+import { routes } from './routes';
 
-Vue.config.productionTip = false
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: routes
+});
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    render: h => h(App),
+    store: store,
+    router: router
+}).$mount('#app');
