73a6fa97043808d305186db685fe09e2c7d4c646
Frida Johansson <frida_mikaela@hotmail.com>
2020-03-31 22:42:40 +0200

    routes

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import Vue from "vue";
 import App from "./App.vue";
-import router from "./router";
+import VueRouter from "vue-router";
 import store from "./store/store";
+import { routes } from "./routes";
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  mode: "history",
+  routes: routes
+});
 
 Vue.config.productionTip = false;
 
--- src/routes.js
+++ src/routes.js
@@ -1,26 +1,13 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import { routes } from "./router/routes";
-
 import Menu from "./views/Menu.vue";
 import About from "./views/About.vue";
 import Order from "./views/Order.vue";
 import LogIn from "./views/Login.vue";
 import Profile from "./views/Profile.vue";
 
-Vue.use(VueRouter);
-
-const routes = [
+export const routes = [
   { path: "/", component: Menu },
   { path: "/login", component: LogIn },
   { path: "/about", component: About },
   { path: "/order", component: Order },
   { path: "/me", component: Profile }
 ];
-
-const router = new VueRouter({
-  mode: "history",
-  routes: routes
-});
-
-export default router;
