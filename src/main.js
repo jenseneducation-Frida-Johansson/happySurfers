849db32c035dfe7012ac404563f65691e1243b25
Frida Johansson <frida_mikaela@hotmail.com>
2020-03-31 22:57:55 +0200

    done

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import VueRouter from "vue-router";
 import store from "./store/store";
-import { routes } from "./routes";
+import { routes } from "./router/routes";
 
 Vue.use(VueRouter);
 
