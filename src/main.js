86fde17423082974cfae15f6e6fa9a5874d96439
Cindy Peng <cindy002587@gmail.com>
2020-04-01 09:04:20 +0200

    change router

--- src/main.js
+++ src/main.js
@@ -1,20 +1,22 @@
-import Vue from 'vue';
-import App from './App.vue';
-import VueRouter from 'vue-router';
-import store from './store/store';
-import { routes } from './routes';
+import Vue from "vue";
+import App from "./App.vue";
+//import VueRouter from 'vue-router';
+import store from "./store/store";
+//import { routes } from './routes';
+import router from "./router";
 
-Vue.use(VueRouter);
+//Vue.use(VueRouter);
 
-const router = new VueRouter({
+/*const router = new VueRouter({
     mode: 'history',
     routes: routes
-});
+});*/
 
 Vue.config.productionTip = false;
 
 new Vue({
-    render: h => h(App),
-    store: store,
-    router: router
-}).$mount('#app');
+  render: h => h(App),
+  store: store,
+  //router: router
+  router: router
+}).$mount("#app");
